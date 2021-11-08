cb7e105efb64c1145e1f34774bfc1d1f1abe4d99
dqoder <2018ucp1097@mnit.ac.in>
2021-11-08 19:37:46 +0530

    volume control added

--- config.def.h
+++ config.def.h
@@ -1,5 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
+#include <X11/XF86keysym.h>
+
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
@@ -73,6 +75,12 @@ static const Layout layouts[] = {
 	{ "[M]",      monocle },
 };
 
+/* audio control */
+static const char *upvol  [] = { "/usr/bin/pactl", "set-sink-volume", "0",    "+5%", NULL };
+static const char *downvol[] = { "/usr/bin/pactl", "set-sink-volume", "0",    "-5%", NULL };
+static const char *mutevol[] = { "/usr/bin/pactl", "set-sink-mute"  , "0", "toggle", NULL };
+
+
 /* key definitions */
 #define MODKEY Mod4Mask
 #define TAGKEYS(KEY,TAG) \
@@ -124,6 +132,10 @@ static Key keys[] = {
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+	// volume control
+	{ 0,                XF86XK_AudioMute,      spawn,          {.v = mutevol} },
+	{ 0,         XF86XK_AudioLowerVolume,      spawn,          {.v = downvol} },
+	{ 0,         XF86XK_AudioRaiseVolume,      spawn,          {.v = upvol  } },
 };
 
 /* button definitions */
