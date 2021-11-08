d7cbb4fe216929814d08894df2a5aefd0757a41c
dqoder <2018ucp1097@mnit.ac.in>
2021-11-08 18:56:44 +0530

    rice: color scheme, MODKEY to super, resizehint, rules

--- config.def.h
+++ config.def.h
@@ -6,43 +6,64 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
+static const char *fonts[]          = { "monospace:size=10", "fontawesome:size=12", "Firacode:size=13" };
 static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_gray1[]       = "#282a36";
+static const char col_gray2[]       = "#111111";
+static const char col_gray3[]       = "#96b5b4";
+static const char col_gray4[]       = "#d7d7d7";
+static const char col_gray5[]       = "#44475a";
+static const char col_cyan []       = "#00ffff";
+static const char col_white[]       = "#ffffff";
+static const char col_black[]       = "#000000";
+static const char col_lime []       = "#bfff00";
+static const char col_red  []       = "#df2331";
+
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
-	[SchemeStatus]  = { col_gray3, col_gray1,  "#000000"  }, // Statusbar right {text,background,not used but cannot be empty}
-	[SchemeTagsSel]  = { col_gray4, col_cyan,  "#000000"  }, // Tagbar left selected {text,background,not used but cannot be empty}
-    [SchemeTagsNorm]  = { col_gray3, col_gray1,  "#000000"  }, // Tagbar left unselected {text,background,not used but cannot be empty}
-    [SchemeInfoSel]  = { col_gray3, col_gray1,  "#000000"  }, // infobar middle  selected {text,background,not used but cannot be empty}
-    [SchemeInfoNorm]  = { col_gray3, col_gray1,  "#000000"  }, // infobar middle  unselected {text,background,not used but cannot be empty}
+        [SchemeNorm]      = { col_gray3, col_gray1,    col_red  },
+        [SchemeSel]       = { col_gray4,  col_cyan,   col_cyan  },
+        [SchemeStatus]    = { col_gray3, col_gray1,   "#000000" }, // Statusbar right {text,background,not used but cannot be empty}
+        [SchemeTagsSel]   = { col_gray1, col_gray3,   "#000000" }, // Tagbar left selected {text,background,not used but cannot be empty}
+        [SchemeTagsNorm]  = { col_gray3, col_gray1,   "#000000" }, // Tagbar left unselected {text,background,not used but cannot be empty}
+        [SchemeInfoSel]   = {  col_cyan, col_gray1,   "#000000" }, // infobar middle  selected {text,background,not used but cannot be empty}
+        [SchemeInfoNorm]  = { col_gray3, col_gray1,   "#000000" }, // infobar middle  unselected {text,background,not used but cannot be empty}
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "", "", "", "", "", "", "", "", ""};
 
 static const Rule rules[] = {
 	/* xprop(1):
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
+	// if tag and swallow both are given --> then tag is used ( swallow would not work,
+	//   even when spawning terminal is on spawn tag of app )
 	/* class     instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
-	{ "Gimp",    NULL,     NULL,           0,         1,          0,           0,        -1 },
-	{ "Firefox", NULL,     NULL,           1 << 8,    0,          0,          -1,        -1 },
-	{ "st",      NULL,     NULL,           0,         0,          1,           0,        -1 },
-	{ NULL,      NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
+	{ "Gimp",     NULL,       NULL,       0,            1,        0,          -1,        -1 },
+	{ "st-256color",  NULL,   NULL,       0,            0,        1,          -1,        -1 },
+	{ NULL,       NULL,   "Event Tester", 0,            0,        0,           1,        -1 }, /* xev */
+	{ "firefox",  NULL,       NULL,       1 << 1,       0,        0,          -1,        -1 },
+	{ "Google-chrome",  NULL, NULL,       1 << 1,       0,        0,          -1,        -1 },
+	{ "Chromium", NULL,       NULL,       1 << 1,       0,        0,          -1,        -1 },
+	{ "Steam",    NULL,       NULL,       1 << 3,       0,        0,          -1,        -1 },
+	{ "VirtualBox Manager",   NULL, NULL, 1 << 4,       0,        0,          -1,        -1 },
+	{ "TelegramDesktop", NULL,NULL,       1 << 5,       0,        0,          -1,        -1 },
+	{ "discord",  NULL,       NULL,       1 << 5,       0,        0,           0,        -1 },
+	{ NULL,  NULL,  "Spotify Free",       1 << 6,       0,        0,          -1,        -1 },
+	{ "obs",      NULL,       NULL,       1 << 7,       0,        0,          -1,        -1 },
+	{ "Thunar",   NULL,       NULL,       1 << 8,       0,        0,          -1,        -1 },
+	{ "Anaconda-Navigator",   NULL, NULL, 1 << 8,       0,        0,          -1,        -1 },
+	{ "Spyder",   NULL,       NULL,       1 << 2,       0,        0,          -1,        -1 },
+	{ "Code",     NULL,       NULL,       1 << 2,       0,        0,          -1,        -1 },
+	{ "Eclipse",  NULL,       NULL,       1 << 2,       0,        0,          -1,        -1 },
 };
 
 /* layout(s) */
 static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
-static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
+static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
 static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
 
 static const Layout layouts[] = {
@@ -53,7 +74,7 @@ static const Layout layouts[] = {
 };
 
 /* key definitions */
-#define MODKEY Mod1Mask
+#define MODKEY Mod4Mask
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
@@ -71,7 +92,7 @@ static const char *termcmd[]  = { "st", NULL };
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY|ShiftMask,             XK_Return,      spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
